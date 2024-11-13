127b3a3fa12e4415b5a13409d11f8546e64d6f8f
Thabang-777 <thabangaphanebusiness1@gmail.com>
2024-11-13 19:57:43 +0200

    use css-variables for dark mode

--- scripts/home.js
+++ scripts/home.js
@@ -1,16 +1,39 @@
 const toggleButtonElement = document.querySelector('.js-toggle');
-const bodyElement = document.querySelector('.body');
+// const bodyElement = document.querySelector('.body');
 const iconElements = document.querySelectorAll('.js-icon');
-console.log(iconElements);
-const mainBodyElement = document.querySelector('.main-content');
-const toggleButtonDiv = document.querySelector('.dark-mode-toggle');
+// const mainBodyElement = document.querySelector('.main-content');
+// const toggleButtonDiv = document.querySelector('.dark-mode-toggle');
+
+const root = document.querySelector(':root');
+const rootStyles = getComputedStyle(root);
+const primaryColor = rootStyles.getPropertyValue('--primary-color')
+const secondaryColor = rootStyles.getPropertyValue('--secondary-color');
+
+let isToggled = -1;
+
+function darkModeToggled (){
+  // the color are swapped here, so they don't need to be in the css-styles
+  if (!isToggled){
+    root.style.setProperty('--primary-color', 'white')
+    root.style.setProperty('--secondary-color', 'black')
+    console.log('normal');
+    isToggled = true;
+  } else if (isToggled = true) {
+    root.style.setProperty('--primary-color', 'black')
+    root.style.setProperty('--secondary-color', 'white')
+    console.log('swapped');
+    isToggled = false;
+  }
+}
 
 toggleButtonElement.addEventListener('click', () => {
   toggleAllIcons();
   toggleButtonElement.classList.toggle('slider-active')
-  bodyElement.classList.toggle('body-dark');
-  mainBodyElement.classList.toggle('main-content-dark');
-  toggleButtonDiv.classList.toggle('dark-mode-toggle-dark');
+  // bodyElement.classList.toggle('body-dark');
+  // mainBodyElement.classList.toggle('main-content-dark');
+  // toggleButtonDiv.classList.toggle('dark-mode-toggle-dark');
+
+  darkModeToggled();
 });
 
 function toggleAllIcons (){
@@ -27,4 +50,16 @@ function updateCalendarIcon (){
 
   dateDisplayElement.innerHTML = day;
 }
+
+function timeUntilMidnight () {
+  const now = new Date();
+  const tommorow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
+  return tommorow - now;
+};
+
 updateCalendarIcon();
+
+setTimeout(() => {
+  updateCalendarIcon();
+  setInterval(updateCalendarIcon, 24 * 60 * 60 * 1000);
+}, timeUntilMidnight());
